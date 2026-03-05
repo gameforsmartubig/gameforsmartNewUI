b799a2bb6260abbcd54d95709ea6002c86baf36a
gameforsmart <gameforsmartubig@gmail.com>
2026-03-05 12:53:41 +0700

    Update user-menu.tsx

--- components/layout2/user-menu.tsx
+++ components/layout2/user-menu.tsx
@@ -30,33 +30,33 @@ export default function UserMenu() {
   };
 
   return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
           <Avatar>
             <AvatarImage src={userData.avatar} alt={userData.name} />
             <AvatarFallback className="rounded-lg">
               {userData.name.substring(0, 2).toUpperCase()}
             </AvatarFallback>
           </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          className="w-(--radix-dropdown-menu-trigger-width) min-w-60"
-          align="end">
-          <DropdownMenuLabel className="p-0">
-            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-              <Avatar>
-                <AvatarImage src={userData.avatar} alt={userData.name} />
-                <AvatarFallback className="rounded-lg">
-                  {userData.name.substring(0, 2).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{userData.name}</span>
-                <span className="text-muted-foreground truncate text-xs">@{userData.username}</span>
-              </div>
-            </div>
-          </DropdownMenuLabel>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      // <DropdownMenu>
+      //   <DropdownMenuTrigger asChild>
+      //   </DropdownMenuTrigger>
+      //   <DropdownMenuContent
+      //     className="w-(--radix-dropdown-menu-trigger-width) min-w-60"
+      //     align="end">
+      //     <DropdownMenuLabel className="p-0">
+      //       <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+      //         <Avatar>
+      //           <AvatarImage src={userData.avatar} alt={userData.name} />
+      //           <AvatarFallback className="rounded-lg">
+      //             {userData.name.substring(0, 2).toUpperCase()}
+      //           </AvatarFallback>
+      //         </Avatar>
+      //         <div className="grid flex-1 text-left text-sm leading-tight">
+      //           <span className="truncate font-semibold">{userData.name}</span>
+      //           <span className="text-muted-foreground truncate text-xs">@{userData.username}</span>
+      //         </div>
+      //       </div>
+      //     </DropdownMenuLabel>
+      //   </DropdownMenuContent>
+      // </DropdownMenu>
   );
 }
